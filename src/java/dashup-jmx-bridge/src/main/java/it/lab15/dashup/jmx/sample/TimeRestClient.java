1a3782e8dda0f8a773d3db93db167db3f8d4ad33
mbonati <marco.bonati@gmail.com>
2013-05-13 23:46:09 +0200

    added jmx client

--- src/java/dashup-jmx-bridge/src/main/java/it/lab15/dashup/jmx/sample/TimeRestClient.java
+++ src/java/dashup-jmx-bridge/src/main/java/it/lab15/dashup/jmx/sample/TimeRestClient.java
@@ -7,6 +7,11 @@ import it.lab15.dashup.client.rest.wreq.TextUpdateRequest;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
+import java.util.Set;
+
+import javax.management.ObjectName;
+
+import com.j256.simplejmx.client.JmxClient;
 
 public class TimeRestClient extends Thread {
 
@@ -16,8 +21,27 @@ public class TimeRestClient extends Thread {
 	
 	public TimeRestClient(String url){
 		this.url = url;
+		testJMXClient();
 	}
 	
+	private void testJMXClient() {
+		try {
+			int port = Integer.parseInt(System.getProperty("com.sun.management.jmxremote.port"));
+			JmxClient client = new JmxClient(1234);
+			// get the set of bean names exported by the JVM
+			Set<ObjectName> objectNameSet = client.getBeanNames();
+			for (ObjectName objName:objectNameSet){
+				System.out.println(objName.getDomain()+ " " + objName.getCanonicalName() + ""); 
+			}
+			// get the start-time in milliseconds
+			long startTimeMillis = (Long)client.getAttribute(new ObjectName("java.lang:type=Runtime"), "StartTime");
+			// run garbage collection
+			client.invokeOperation(new ObjectName("java.lang:type=Memory"), "gc");
+		} catch (Exception ex){
+			ex.printStackTrace();
+		}
+	}
+
 	public void run(){
 		TextUpdateRequest textUpdate = new TextUpdateRequest("text.simpleExample");
 		GaugeUpdateRequest gaugeUpdate = new GaugeUpdateRequest("gauge.simpleExample");
