293cd04f3ae7e65cb4a3c91911acff201d334849
mbonati <marco.bonati@gmail.com>
2013-05-27 20:57:45 +0200

    added first save/load dashboard

--- src/web/app/public/js/dashup/dashup.js
+++ src/web/app/public/js/dashup/dashup.js
@@ -1,6 +1,8 @@
 var gridster;
 var socket;
 
+var currentDashboardId;
+
 /**
  * Initialize the Dashup app
  */
@@ -18,6 +20,8 @@ function initDashup(){
   	gridster = $(".gridster ul").gridster().data('gridster');
 
  	socket = io.connect();
+
+ 	registerBaseWidgets();
 }
 
 function onDragStop(event, ui){
@@ -60,8 +64,8 @@ function serializeDashboard(){
 	})
 }
 
-function loadDashboard(dashboardId){
-	console.log("load dashboard called for: "+dashboardId);
+function loadDashboardData(dashboardId){
+	console.log("loadDashboardData called for: "+dashboardId);
 	$.ajax({
 		type: 'post',
 		url : './loadDashboard',
@@ -69,11 +73,50 @@ function loadDashboard(dashboardId){
 		data: { 'dashboardId' : dashboardId },
 		success: function(data, textStatus, xhr){
 			console.log("loadDashboard success: " + textStatus + " -> " +data);
+			loadDashboard(data);
 		},
 		error: function(xhr, textStatus, errorThrown) {
 			console.log("load dashboard error: " + errorThrown);
         }
 	})
+}
+
+function loadDashboard(dashboardData){
+	console.log("loadDashboard>> called for " + JSON.stringify(dashboardData));
+
+	currentDashboardId = dashboardData.dashboardId;
+
+	//check validity of message
+	if (!dashboardData.dashboard){
+		return;
+	}
+	if (!dashboardData.dashboard.widgets){
+		return;
+	}
+
+	for (i=0;i<dashboardData.dashboard.widgets.length;i++){
+		loadWidget(dashboardData.dashboard.widgets[i]);
+	}
+
+}
+
+function loadWidget(widgetData){
+	console.log("loadWidget called for " + JSON.stringify(widgetData));
+	var widgetInfo = getWidgetInfo(widgetData.widgetType);
+	if (!widgetInfo){
+		console.log("unable to find widgetInfo for type " + widgetData.widgetType);
+		return;
+	} else {
+		var initCfg = {
+			title: widgetInfo.defaults.title,
+			template: widgetInfo.template,
+			size: widgetInfo.defaults.size,
+			position: [widgetData.widgetPosition.col,widgetData.widgetPosition.row]
+		};
+		console.log("loadWiget for " + JSON.stringify(initCfg));
+		createWidget(initCfg);
+	}
+
 }
 
 function lockDashboard(){
@@ -101,6 +144,65 @@ function uniquieId(){
 
 	return components.join("");
 }
+
+
+/******** Widget Registry **///
+var widgetRegistry = {};
+function registerWidget(widgetInfo){
+	widgetRegistry[widgetInfo.type] = widgetInfo;	
+}
+function getWidgetInfo(widgetType){
+	return widgetRegistry[widgetType];
+}
+/******** END Widget Registry **///
+
+
+
+
+function registerBaseWidgets(){
+	registerClockWidget();	
+	registerGaugeWidget();
+	registerTextWidget();
+}
+
+
+function registerClockWidget(){
+	var wInfo = {
+		type : "dashup.base.clock",
+		template: 'widgets/clock.widget.html',
+		defaults: {
+			title: "Clock",
+			size: [2,2]
+		}
+	};
+	registerWidget(wInfo);
+}
+
+function registerGaugeWidget(){
+	var wInfo = {
+		type : "dashup.base.gauge",
+		template: 'widgets/gauge.widget.html',
+		defaults: {
+			title: "Gauge",
+			size: [2,2]
+		}
+	};
+	registerWidget(wInfo);
+}
+
+function registerTextWidget(){
+	var wInfo = {
+		type : "dashup.base.text",
+		template: 'widgets/text.widget.html',
+		defaults: {
+			title: "Text",
+			size: [4,1]
+		}
+	};
+	registerWidget(wInfo);
+}
+
+
 
 /**
  * Clock Widget 
@@ -177,7 +279,9 @@ function createWidget(initConfig){
 							   };
 			gridster.add_widget('<li class="new">'+compiled(widgetConfig)+'</li>',
 			 					initConfig.size[0], 
-			 					initConfig.size[1]);
+			 					initConfig.size[1],
+			 					initConfig.position[0],
+			 					initConfig.position[1]);
 		});
 
 }
